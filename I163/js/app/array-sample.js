bdc281a0cbc38ed902988732ca3bb45802f35eac
Jordan Drake <jordan@jdrake.co.uk>
2014-06-10 16:26:32 +0100

    Finished the standard gameplay, needs some refactoring and maybe some prettying up.

--- I163/js/app/array-sample.js
+++ I163/js/app/array-sample.js
@@ -1,10 +1,18 @@
 define(function () {
 
+  Array.prototype.randomIndex = function () {
+    return Math.floor(Math.random() * this.length);
+  };
+
+  Array.prototype.randomElement = function () {
+    return this[this.randomIndex()];
+  };
+
   Array.prototype.randomSample = function (n) {
     var sample = [];
     for(var i=0; i < n; i++) {
       do {
-        var randomElement = this[Math.floor(Math.random() * this.length)];
+        var randomElement = this.randomElement();
       } while (sample.indexOf(randomElement) > -1 )
       sample.push(randomElement);
     }
---
+++ I163/js/app/mastermind.js
@@ -0,0 +1,47 @@
+define(['levenshtein', 'app/array-sample'], function (levenshtein) {
+  return function Mastermind(dictionary, amount, upperLimit, lowerLimit, difficulty) {
+
+    this.triedWords = [];
+    this.words = [];
+    this.answer;
+
+    this.attempts = 4;
+    this.solved = false;
+
+    (function () {
+      var n = 7;
+      var nCharWords = dictionary.filter(function (word) {
+        return word.length === n;
+      });
+
+      this.answer = nCharWords.randomElement();
+      this.words = nCharWords.filter(function (word) {
+        var distance = levenshtein(word, this.answer);
+        return distance > 0 && distance < 5;
+      }.bind(this)).randomSample(9);
+
+      this.words.splice(this.words.randomIndex(), 0, this.answer);
+    }).bind(this)();
+
+    this.try = function (selectedWord) {
+      if (this.attempts > 0) {
+        if (selectedWord === this.answer) {
+          this.solved = true;
+        } else {
+          var sameChars = 0;
+          for (var i = 0; i < selectedWord.length; i++) {
+            if (this.answer[i] && selectedWord[i] && this.answer[i] === selectedWord[i]) {
+              sameChars++;
+            }
+          }
+          this.triedWords.push({
+            word: selectedWord,
+            similarity: sameChars
+          });
+          this.words.splice(this.words.indexOf(selectedWord), 1);
+          this.attempts--;
+        }
+      }
+    }
+  };
+});
